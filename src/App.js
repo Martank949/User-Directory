987421df525b9116c805ec008e589dbf617c6e65
Gargantua <martinzb949@gmail.com>
2021-02-08 18:57:06 -0800

    made some additions to my app.js & index.html for bootstrap to work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import Card from "./components/Card";
 import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
-import SearchResultContainer from "./components/SearchResultContainer/SearchResultContainer";
+// import SearchResultContainer from "./components/SearchResultContainer/SearchResultContainer";
 
 function App() {
 	return (
 		<div className="container">
 			<Jumbotron />
-			<Navbar />
 			<Navbar />
 			<Card />
 		</div>
